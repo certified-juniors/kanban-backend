371647436827f72281a0d54ba3fd70ea243afeb6
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-09-27 22:44:03 +0300

    Added swagger for authentication

--- internal/http-server/handlers/user.go
+++ internal/http-server/handlers/user.go
@@ -4,11 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"kanban/internal/domain/models"
+	resp "kanban/internal/lib/api/response"
 	"kanban/internal/lib/api/validation"
 	"kanban/internal/lib/logger/sl"
 	"net/http"
 )
 
+// Login Авторизация пользователя
+// @Summary Авторизировать пользователя
+// @Description Авторизация пользователя
+// @Tags User
+// @Router /user/login [post]
+// @Param email body string true "Почта"
+// @Param password body string true "Пароль"
+// @Success 200 {object} models.AuthTokens "OK"
+// @Failure 400 {object} resp.Response "Bad request"
+// @Failure 500 {object} resp.Response "Internal server error"
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	var op string = "User.Login"
 	log := h.log.With("op", op)
@@ -18,23 +29,34 @@ func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&credentials)
 	if err != nil {
 		log.With("op", op).With("error", err).Error("error decoding body")
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		resp.WriteJSONResponse(w, http.StatusBadRequest, "invalid input parameters", nil)
 		return
 	}
 
 	authTokens, err := h.userService.Login(r.Context(), credentials)
 	if err != nil {
 		log.With("op", op).With("error", err).Error("error calling Login")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		resp.WriteJSONResponse(w, http.StatusInternalServerError, "internal server error", nil)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(authTokens)
+	resp.WriteJSON(w, http.StatusOK, authTokens)
 }
 
-// TODO: Add human-readable errors
+// Register Зарегистрировать пользователя
+// @Summary Регистрация пользователя
+// @Description Регистрация пользователя
+// @Tags User
+// @Router /user/register [post]
+// @Param name body string true "Имя"
+// @Param surname body string true "Фамилия"
+// @Param middle_name body string false "Отчество"
+// @Param email body string true "Почта"
+// @Param password body string true "Пароль"
+// @Param repeat_password body string true "Повторите пароль"
+// @Success 201 {object} models.AuthTokens "Created"
+// @Success 400 {object} resp.Response "Bad request"
+// @Success 500 {object} resp.Response "Internal server error"
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	var op string = "User.Register"
 	log := h.log.With("op", op)
@@ -43,25 +65,23 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&credentials)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		resp.WriteJSONResponse(w, http.StatusBadRequest, "invalid input parameters", nil)
 		return
 	}
 
 	if validationErrors := validation.ValidateStruct(credentials); len(validationErrors) > 0 {
 		log.With("credentials", credentials).Error("invalid input parameters", sl.Err(fmt.Errorf("validation errors")))
-		http.Error(w, "invalid input parameters", http.StatusBadRequest)
+		resp.WriteJSONResponse(w, http.StatusBadRequest, "invalid input parameters", nil)
 		return
 	}
 
 	authTokens, err := h.userService.Register(r.Context(), credentials)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		resp.WriteJSONResponse(w, http.StatusInternalServerError, "internal server error", nil)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(authTokens)
+	resp.WriteJSON(w, http.StatusOK, authTokens)
 }
 
 func (h *Handler) Logout(w http.ResponseWriter, r *http.Request) {
--- internal/http-server/http-server.go
+++ internal/http-server/http-server.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "kanban/docs"
 )
 
 // @title Kaban API
